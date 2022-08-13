93d673001560aa70509f594d6e326d945f8879b2
Amado David Ortiz Gallardo <aortigal@emeal.nttdata.com>
2022-08-13 18:14:55 -0500

    structure base

---
+++ src/main/java/com/bank/mspayment/services/impl/ClientImpl.java
@@ -0,0 +1,26 @@
+package com.bank.mspayment.services.impl;
+
+import com.bank.mspayment.models.utils.ResponseClient;
+import com.bank.mspayment.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ClientImpl implements ClientService {
+
+    @Qualifier("getWebClientC")
+    @Autowired
+    WebClient webClient;
+
+    @Override
+    public Mono<ResponseClient> findByCode(String id)
+    {
+        return webClient.get()
+                .uri("/api/client/"+ id)
+                .retrieve()
+                .bodyToMono(ResponseClient.class);
+    }
+}
